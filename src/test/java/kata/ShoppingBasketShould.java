ecba0a8d8c97243fe1c84f1952e889b9f99683a7
iagardi <garbigz@gmail.com>
2022-02-09 13:31:31 +0000

    rollback

--- src/test/java/kata/ShoppingBasketShould.java
+++ src/test/java/kata/ShoppingBasketShould.java
@@ -1,18 +1,12 @@
 package kata;
 
 import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 public class ShoppingBasketShould {
 
     @Test void
     add_a_item_into_the_basket (){
 
-        var basketMock = mock(ShoppingBasketService.class);
-        UserID user = new UserID(1);
-        ProductID product = new ProductID(1);
-        basketMock.addItem( user, product,1);
-        verify(basketMock).addItem(user, product,1);
+
     }
 }
