fb1e81240349e17377763c31688fbab37e4eb668
Simon <simonbesteiro@gmail.com>
2022-02-09 19:03:52 +0100

    WIP

--- src/main/java/kata/ShoppingBasket.java
+++ src/main/java/kata/ShoppingBasket.java
@@ -20,14 +20,22 @@ public class ShoppingBasket implements ShoppingBasketService {
         StringBuilder output = new StringBuilder(" - Creation date (of the shopping basket)\n");
         var list = shoppingRepository.getFor(userId);
         for (Purchase purchase : list) {
+            //TODO Get product information by productID and use it on the output
             output.append("    - ");
             output.append(purchase.amount());
             output.append(" x ");
-            output.append(purchase.productID().product());
+            output.append("mabe based on"+purchase.productID().product());
+            output.append("   // ");
+            output.append(purchase.amount());
+            output.append(" x ");
+            output.append("----");
+            output.append(" = £");
+            output.append("----");
             output.append("\n");
         }
 
-
+        output.append("Total: £");
+        output.append("----");
         return output.toString();
     }
 }
