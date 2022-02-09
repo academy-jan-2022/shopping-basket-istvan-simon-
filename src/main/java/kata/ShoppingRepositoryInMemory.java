529bf9457f35a8bc467642bd9332ff76d79a0962
Simon <simonbesteiro@gmail.com>
2022-02-09 18:12:15 +0100

    green

--- src/main/java/kata/ShoppingRepositoryInMemory.java
+++ src/main/java/kata/ShoppingRepositoryInMemory.java
@@ -1,18 +1,26 @@
 package kata;
 
-import java.util.Dictionary;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class ShoppingRepositoryInMemory implements ShoppingRepository {
-    private final HashMap<UserID,Purchase> storage = new HashMap<UserID,Purchase>();
+    private HashMap<UserID, List<Purchase>> storage = new HashMap<UserID,List<Purchase>>();
     @Override
     public void addPurchase(UserID userID,ProductID product, int quantity) {
-        storage.put(userID, new Purchase(product, quantity));
-    }
+        if (storage.get(userID) == null){
+            storage.put(userID,List.of(new Purchase(product, quantity)));
+            return;
+        }
+        List<Purchase> oldList = storage.get(userID);
+        List<Purchase> list= new ArrayList<>();
+        list.addAll(oldList);
+        list.add(new Purchase(product, quantity));
 
+        storage.put(userID, list);
+    }
     @Override
     public List<Purchase> getFor(UserID userID) {
-        return List.of(storage.get(userID));
+        return storage.get(userID);
     }
 }
